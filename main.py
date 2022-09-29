e7df1e1d464843fd014fcfb65b888d0b1d1063fa
tessierl <tessierl@educ.college-montreal.qc.ca>
2022-09-29 10:44:14 -0400

    boucles et conditions ajoutées.

--- main.py
+++ main.py
@@ -5,13 +5,24 @@
 
 import random
 
-ordi = random.randint(0, 100)
-nb_essai = 0
+boucle_jeu = True
+tours = True
+while boucle_jeu:
+    ordi = random.randint(0, 100)
+    nb_essai = 0
 
-print("J’ai choisi un nombre au hasard entre borne_minimale et borne_maximale. À vous de le deviner...")
-essai = input("Entrez votre essai :")
+    print("J’ai choisi un nombre au hasard entre borne_minimale et borne_maximale. À vous de le deviner...")
+    while tours:
+        essai = int(input("Entrez votre essai :"))
 
-if essai < ordi :
-    print("Mauvais choix, le nombre que j'ai choisi est plus petit.")
-    essai
-    nb_essai =+1
+        if essai > ordi :
+            print("Mauvais choix, le nombre que j'ai choisi est plus petit.")
+            nb_essai += 1
+            tours = False
+        elif essai < ordi :
+            print("Mauvais choix, le nombre que j'ai choisi est plus grand.")
+            nb_essai += 1
+            tours = False
+        elif essai == ordi :
+            print("Bravo! Bonne réponse! Vous avez réussi en ")
+            quit = input("Voulez-vous recommencer (y/n)?")
